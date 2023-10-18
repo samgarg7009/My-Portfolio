cbb5cd9df5d6314f52bb451679626e1cb4816504
samgarg7009 <samikshagarg321@gmail.com>
2023-10-18 23:15:25 +0530

    first commit

---
+++ my_portfolio/src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import styles from './App.module.css'
+
+const App =()=>{
+
+  return (
+    <>
+    <div className={styles.App}>
+      hello world
+    </div>
+    </>
+  )
+}
+
+export default App
